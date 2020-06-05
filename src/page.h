880365278200c93892a8b32ae8d9b2bcf8a0d5c7
Harrison Hesslink <harrisonhesslink24@gmail.com>
2020-06-05 16:24:44 -0700

    Add SN Functionality pt8

--- src/page.h
+++ src/page.h
@@ -1332,7 +1332,7 @@ public:
         }
 
         // get block size in bytes
-        uint64_t blk_size = core_storage->get_db().get_block_weight(block_height);
+        uint64_t blk_size = core_storage->get_db().get_block_weight(_blk_height);
 
         // miner reward tx
         transaction coinbase_tx = blk.miner_tx;
@@ -4784,9 +4784,6 @@ public:
 
                 // get block size in bytes
             double blk_size = core_storage->get_db().get_block_weight(i);
-
-            crypto::hash blk_hash = core_storage->get_block_id_by_height(i);
-
 
             // get block age
             pair<string, string> age = get_age(local_copy_server_timestamp, blk.timestamp);
